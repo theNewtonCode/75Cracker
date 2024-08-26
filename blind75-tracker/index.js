464773b9549b7db8aefd5199b30a836fdf093bcf
Abhyuday Rai <abhyudayrai03@gmail.com>
2024-08-26 08:33:16 +0530

    updated for hosting

--- blind75-tracker/index.js
+++ blind75-tracker/index.js
@@ -21,7 +21,9 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
-app.use(express.json());
+app.get("/", (req, res)=>{
+  res.json("Server Home");
+})
 
 // Routes
 const trackerRoutes = require('./routes/tracker');
